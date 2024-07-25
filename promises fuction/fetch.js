6469d06cbf9ed84f3d4d089d8b84eb0a5177e7d1
DHRUV-298 <pateldhruv2023.katargam@gmail.com>
2024-07-25 19:58:49 +0530

    25

--- promises fuction/fetch.js
+++
@@ -1,9 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-.then((respons) => respons.json())
-.then((respons)=>{
-    console.log(respons.title);
-    document.write(respons.title)
-})
-.catch((eror) =>{
-    console.log(eror);
-})
